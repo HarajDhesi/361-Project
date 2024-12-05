061cfc4d1c60881473f95f88465dca56a01d6354
Haraj S. Dhesi <107383028+HarajDhesi@users.noreply.github.com>
2024-12-05 06:50:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import imdb
+import zmq
+import json
 
 def main():
-
-    ia = imdb.Cinemagoer()
 
     # Intialize list datastructure
     movies_list = []
@@ -11,20 +10,20 @@ def main():
     print("*** Welcome to Movie Hub ***")
     print("Save all of your favorite movies in one place!")
 
-
     user_input = 0
-    while user_input != 5:
+    while user_input != 7:
 
         print("\nCOMMANDS:")
         print("1) Type '1' to add a movie")
         print("2) Type '2' to lookup a movie")
         print("3) Type '3' to view all movies")
-        print("4) Type '4' for help on how to use this program")
-        print("5) Type '5' to quit")
+        print("4) Type '4' to acess movie reminder services")
+        print("5) Type '5' to get an AI movie recommendation")
+        print("6) Type '6' to view the most trending movies")
+        print("7) Type '7' for help on how to use this program")
+        print("8) Type '8' to quit")
 
         user_input = int(input("\nSelect command: "))
-        
-        # user_input = int(input("\n[1/2/3/4]: "))
 
         if user_input == 1:
             print("Adding a movie ...\n")
@@ -47,19 +46,22 @@ def main():
 
             elif adding_method == 2:
                 imdb_id = input("Please enter the IMDb movie id: ")
-                movie = ia.get_movie(imdb_id)
-                movieName = movie['title']
-                # directorName = ""
-                # for item in movie['directors']:
-                #     directorName += " " + str(item)
-                directors = movie['directors']
-                directorName = ", ".join(director['name'] for director in directors)
-                releaseYear = movie['year']
-                genreList = movie['genres']
-                movieGenre = ", ".join(genreList)
-                print('\n')
+                context = zmq.Context()
+                print("\nClient attempting to connect to IMDb ID Search Microservice...")
+                socket = context.socket(zmq.REQ)
+                socket.connect("tcp://localhost:5561") 
+
+                print(f"Sending a request to the IMDB ID Search Microservice for IMDb id: {imdb_id}.")
+                socket.send_string(imdb_id)
+
+                imdb_response = socket.recv_json()
+                movieName = imdb_response.get('movie_name')
+                directorName = imdb_response.get('directors')
+                releaseYear = imdb_response.get('release_year')
+                movieGenre = imdb_response.get('genre')
+
                 print(f"Movie Name: {movieName}, Directors: {directorName}, Release Year: {releaseYear}, Genre: {movieGenre}")
-                correct_details = input("Are the movie details correct? (Y/N): ")
+                correct_details = input("\nAre the movie details correct? (Y/N): ")
 
                 if correct_details == "Y":
                     movies_list.append([movieName, directorName, releaseYear, movieGenre])
@@ -78,7 +80,6 @@ def main():
                         print("\n")
                         print(f"Movie Name: {movie[0]}, Directors: {movie[1]}, Release Year: {movie[2]}, Genre: {movie[3]}")
 
-                        # implement deleting logic here
                         print("\nCOMMANDS:")
                         print("1) Type '1' to delete a movie")
                         print("2) Type '2' to return back to the home page")
@@ -102,13 +103,9 @@ def main():
             if len(movies_list) == 0:
                 print("You currently have no movies saved to your catalog. Please add a movie first by selecting the add movie option.")
             else:
-                # need to change so only the movie title is displayed
                 for i in range(len(movies_list)):
                     print(f"{i+1}. {movies_list[i][0]}")
 
-                    # print(f"{i+1}. Movie Name: {movies_list[i][0]}, Directors: {movies_list[i][1]}, Release Year: {movies_list[i][2]}, Genre: {movies_list[i][3]}")
-
-                # have to implement logic to allow user to select a movie from the list of movies to get more details about it.
                 print("\nCOMMANDS:")
                 print("1) Type '1' to lookup a movie")
                 print("2) Type '2' to return back to the home page")
@@ -122,7 +119,6 @@ def main():
                             print("\n")
                             print(f"Movie Name: {movie[0]}, Directors: {movie[1]}, Release Year: {movie[2]}, Genre: {movie[3]}")
 
-                            # implement deleting logic here
                             print("\nCOMMANDS:")
                             print("1) Type '1' to delete a movie")
                             print("2) Type '2' to return back to the home page")
@@ -139,28 +135,147 @@ def main():
 
                                 else:
                                     print("You have aborted the deletion request.")
-                
-            # for registered_movie in movies_list:
-            #     print(f"Moive Name: {registered_movie[0]}, Directors: {registered_movie[1]}, Release Year: {registered_movie[2]}, Genre: {registered_movie[3]}")
-
 
         elif user_input == 4:
+            print("1) Type '1' to create a movie reminder")
+            print("2) Type '2' to delete a previously created movie reminder")
+            print("3) Type '3' to return back to the home page")
+            request_type = int(input("\nSelect command: "))
+
+            if request_type == 1:
+                
+                recipient_email = None
+                recipient_phone = None
+
+                user_selected_movie = input("Please enter the tile of the movie you would like to be reminded of?: ")
+                message = (f"Hey! Don't forget to watch {user_selected_movie}. This reminder is brought to you by Moviehub.")
+                
+                reminder_type = int(input("Would you like to recieve this reminder via email (1), sms (2), or both (3)? 1/2/3: "))
+                if reminder_type == 1:
+                    reminder_delivery_method = 'email'
+                    recipient_email = input("Please provide the email address at which you would like to recieve the reminder: ")
+                elif reminder_type == 2:
+                    reminder_delivery_method = 'sms'
+                    recipient_phone = input("Please provide the phone number at which you would like to recieve the reminder. (Example format: 15551234567): ")
+                elif reminder_type == 3:
+                    reminder_delivery_method = 'both'
+                    recipient_email = input("Please provide the email address at which you would like to recieve the reminder: ")
+                    recipient_phone = input("Please provide the phone number at which you would like to recieve the reminder. (Example format: 15551234567): ")
+
+                hours = int(input("In how many hours and minutes would you like to recieve this reminder? Hours first: "))
+                minutes = int(input("In how many minutes would you like to recieve this reminder?: "))
+
+                reminder_id_choice = input("Would you like to proivde a reminder id? Y/N: ")
+                if reminder_id_choice == "Y":
+                    reminder_id = str(input("Please enter an id: "))
+                else:
+                    reminder_id = None
+
+                reminder_request(
+                    microservice_request= "create",
+                    reminder_id= reminder_id,
+                    reminder_delivery_method= reminder_delivery_method,
+                    hours= hours,
+                    minutes= minutes,
+                    email_subject= "Movie Reminder",
+                    message= message,
+                    recipient_email= recipient_email,
+                    recipient_phone= recipient_phone
+                )
+
+            elif request_type == 2:
+                reminder_id = input("Please enter the reminder id: ")
+
+                reminder_request(
+                    microservice_request= "delete",
+                    reminder_id= reminder_id
+                )
+
+        elif user_input == 5:
+            movie_name = input("Please enter the name of the movie you would like your recommendation to be based on: ")
+
+            context = zmq.Context()
+            print("\nClient attempting to connect to the AI Recommendation Microservice...")
+            socket = context.socket(zmq.REQ)
+            socket.connect("tcp://localhost:5560")
+
+            keep_recommending = True
+            while keep_recommending:
+
+                print("\nSending a recommendation request to the AI Recommendation Microservice.")
+                socket.send_string(movie_name)
+
+                recommendation_response = socket.recv_string()
+                print(f"Response from AI Recommendation Microservice recieved: {recommendation_response}")
+
+                retry = input("\nWould you like another recommendation based on the same movie? (Y/N): ")
+                if retry != "Y":
+                    keep_recommending = False
+
+        elif user_input == 6:
+            print("1) Type '1' to get Top Trending Movies of the Day.")
+            print("2) Type '2' to get Top Trending Movies of the Week.")
+            
+            choice = int(input("\nSelect command: "))
+            time_period = "day" if choice == 1 else "week"
+
+            number_of_movies = int(input("PLease enter the number of movies you want to see: "))
+
+            context = zmq.Context()
+            print("\nClient attempting to connect to the Trending Movies Microservice...")
+            socket = context.socket(zmq.REQ)
+            socket.connect("tcp://localhost:5557")
+            
+            print(f"Sending a request for the Top {number_of_movies} Trending Movies of the {time_period} to the Trending Movies Microservice.")
+            socket.send_json({
+                "time_period": time_period,
+                "number_of_movies": number_of_movies
+            })
+
+            popular_response = socket.recv_json()
+            print("Response from the Trending Movies Microservice:")
+            print(f"Top {number_of_movies} Trending Movies of the {time_period}:")
+            for idx, movie in enumerate(popular_response, start=1):
+                print(f"{idx}, {movie['title']}")
+
+        elif user_input == 7:
             print("\nMovieHub serves as a catalog to store all your favorite movies.")
             print("You can add movies to your catalog by selecting the 'add a movie' option.")
             print("If a movie was mistakenly added or a data error is present, a movie can be deleted by looking up the movie, and selecting the 'delete a movie' option.")
             print("Be aware that this wil result in the movie being permanetly deleted.")
+            print("MovieHub also offers the option to set email and/or sms movie reminders, recieve AI movie recommendations, and view the most trending movies of the day/week.")
                 
-        elif user_input == 5:
+        elif user_input == 8:
             print("\nTerminating the program ...")
 
     print("Thank you for using MovieHub! Goodbye!\n")
 
 
-    # Idea:
-    # On the start of the app, give user the commands to View Catalog or Exit
-    # when viewing catalog, it will display all movies or display a helpful promt that tells how to add a movie
-        # command options will be
-        # 1) Add a movie, 2) View a movie 3) Return to home 4) Quit
+def reminder_request(microservice_request, reminder_id=None, reminder_delivery_method=None, hours=None, minutes=None, email_subject=None, message=None, recipient_email=None, recipient_phone=None):
+    context = zmq.Context()
+    print("Client attempting to connect to Reminder Microservice...")
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
+
+    reminder_message = {
+        'microservice_request': microservice_request,
+        'reminder_id': reminder_id,
+        'reminder_delivery_method': reminder_delivery_method,
+        'hours': hours,
+        'minutes': minutes,
+        'message': message,
+        'email_subject': email_subject,
+        'provider_email': "moviehubalerts@gmail.com",
+        'recipient_email': recipient_email,
+        'recipient_phone': recipient_phone
+    }
+
+    reminder_message = {key: value for key, value in reminder_message.items() if value is not None}
+
+    print("Sending a request to the Reminder Microservice")
+    socket.send_json(reminder_message)
+    response = socket.recv_json()
+    print(f"Response from Reminder Microservice: {json.dumps(response, indent=2)}")
 
 
 if __name__ == "__main__":
